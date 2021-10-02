aac9e5e52961b7a0460904ce38c15deb8bd0b07e
witoivaro <witodivaro@gmail.com>
2021-10-02 13:28:39 +0300

    ADD: Container

---
+++ src/react-patterns/container/Container.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import ReactPattern from "../../components/ReactPattern";
+
+const fetchElements = async () =>
+  new Promise((resolve) => {
+    const elements = [1, 2, 3, 4, "text"];
+
+    setTimeout(() => {
+      resolve(elements);
+    }, 1000);
+  });
+
+const DumbComponent = ({ elements }) => {
+  return (
+    <div>
+      {elements.map((element) => (
+        <span style={{ marginLeft: 10 }}>{element}</span>
+      ))}
+    </div>
+  );
+};
+
+const ContainerComponent = () => {
+  const [elements, setElements] = useState([]);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const elements = await fetchElements();
+      setElements(elements);
+    };
+
+    fetch();
+  }, []);
+
+  return <DumbComponent elements={elements} />;
+};
+
+const Container = () => {
+  return (
+    <ReactPattern name="Container">
+      <ContainerComponent />
+    </ReactPattern>
+  );
+};
+
+export default Container;
